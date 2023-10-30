efa66012d74122f14d3d90a5b909c38d78cd1519
storyofhis <azizi.maula@gmail.com>
2023-10-30 16:38:46 +0700

    pull dari luthfiyyah

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,5 +55,10 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+//    GSON
+    implementation("com.google.code.gson:gson:2.8.9")
+//    API Request
+    implementation("com.android.volley:volley:1.2.1")
+
     implementation("org.tensorflow:tensorflow-lite:2.7.0")
 }
